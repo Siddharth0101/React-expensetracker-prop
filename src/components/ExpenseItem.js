c7d02b6e3d5d752cfcb2ecee6c88d695ee875c03
Siddharth0101 <147489528+Siddharth0101@users.noreply.github.com>
2023-12-08 15:18:25 +0530

    update prop

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,11 @@
 import "./ExpenseItem.css";
 function ExpenseItem(prop) {
-  const expenseDate = prop.date;
-  const expenseTitle = prop.title;
-  const expenseAmount = prop.amount;
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{prop.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expenseAmount}</div>
+        <h2>{prop.title}</h2>
+        <div className="expense-item__price">{prop.amount}</div>
       </div>
     </div>
   );
